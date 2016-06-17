512c200ed99e928bdad539cddc7fdd5f8d780c2f
Bastian Mager <bastian.mager@smail.inf.h-brs.de>
2016-06-17 12:55:11 +0200

    corrected render if element available code

--- components/ccm.ranking-summary.js
+++ components/ccm.ranking-summary.js
@@ -20,49 +20,47 @@ ccm.component( {
 		var self = this;
 		
 		this.render = function (callback) {
-			var render_element = self.render_element;
-
-			if(render_element) {
+	
+			this.store.get(this.store_dataset, function(data) {
 				
-				this.store.get(this.store_dataset, function(data) {
-					
-					var corpus = data[self.store_corpus_key];
-					var ranks = data[self.store_ranking_key];
-					var summary_size = corpus.length*self.compress_factor;
-					ranks = Object.keys(ranks);
-					
-					// sort by ranking
-					ranks.sort(function(a,b) {
-						return ranks[a] <= ranks[b];
-					});
-					
-					// select
-					ranks = ranks.slice(0,summary_size);
-					
-					// sort by appearance
-					ranks = ranks.sort(function(a, b) {
-						return a - b;
-					});
-					
-					// generate summary
-					var summary = "";
-					for(var rank of ranks) {
-						summary += corpus[rank] + " ";
-					}
-					
-					// render tagged data
-					if(self.render_element) {
-						self.render_element.html(ccm.helper.html(summary));
-					}
-					
-					// store sentences
-					var storable = new Object();
-					storable['key'] = self.store_dataset;
-					storable[self.store_dst_key] = summary;
-					self.store.set(storable, callback);
+				var corpus = data[self.store_corpus_key];
+				var ranks = data[self.store_ranking_key];
+				var summary_size = corpus.length*self.compress_factor;
+				if(summary_size < 1) summary_size = 1;
+				
+				ranks = Object.keys(ranks);
+				
+				// sort by ranking
+				ranks.sort(function(a,b) {
+					return ranks[a] <= ranks[b];
 				});
-			}
-			if(callback) callback();
+				
+				// select
+				ranks = ranks.slice(0,summary_size);
+				
+				// sort by appearance
+				ranks = ranks.sort(function(a, b) {
+					return a - b;
+				});
+
+				// generate summary
+				var summary = "";
+				for(var rank of ranks) {
+					summary += corpus[rank] + " ";
+				}
+				
+				// render tagged data
+				if(self.render_element) {
+					self.render_element.html(ccm.helper.html(summary));
+				}
+				
+				// store sentences
+				var storable = new Object();
+				storable['key'] = self.store_dataset;
+				storable[self.store_dst_key] = summary;
+				self.store.set(storable, callback);
+			});
+
 		}
 		
 	}
